18de0280bb5207ac4d20b90f4496c9176b984189
yann <yann.hortance@gmail.com>
2020-05-08 13:12:57 +0200

    Add test for MathLibFactory

---
+++ ColumbaChallange.MathLib/ColumbaChallange.MathLib.Tests/MathLibFactoryTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ColumbaChallange.MathLib.Tests
+{
+    public class MathLibFactoryTests
+    {
+        private MathLibFactory _mathLibFactory;
+        public MathLibFactoryTests()
+        {
+            _mathLibFactory = new MathLibFactory();
+        }
+
+        [Fact]
+        public void RecursiveShouldReturnMathLibInstance()
+        {
+            var mathLib = _mathLibFactory.CreateMathLib(MathLibType.Recursive);
+            Assert.IsType<MathLib>(mathLib);
+        }
+        [Fact]
+        public void RecursiveShouldReturnMathLibV2Instance()
+        {
+            var mathLib = _mathLibFactory.CreateMathLib(MathLibType.ListFunction);
+            Assert.IsType<MathLibV2>(mathLib);
+        }
+    }
+}
