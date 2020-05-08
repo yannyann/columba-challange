2d3ba9c7afb6d0ea497b8a5375474ac71a573681
yann <yann.hortance@gmail.com>
2020-05-08 13:04:21 +0200

    implement MathLibV2

--- ColumbaChallange.MathLib/ColumbaChallange.MathLib/MathLibV2.cs
+++ ColumbaChallange.MathLib/ColumbaChallange.MathLib/MathLibV2.cs
@@ -7,8 +7,11 @@ namespace ColumbaChallange.MathLib
     {
         public long Factorial(int n)
         {
-            throw new NotImplementedException();
-            //return Enumerable.Range(0, n).Aggregate((i, j) => i*j);
+            if (n < 0)
+            {
+                throw new ArgumentException($"{nameof(n)} cannot be strictly negative.");
+            }
+            return n == 0 ? 1 : Enumerable.Range(1, n).Aggregate((i, j) => i * j);
         }
     }
 }
