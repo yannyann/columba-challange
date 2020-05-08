a2404e2332e5f926c6686583630b22d388a58dc2
yann <yann.hortance@gmail.com>
2020-05-08 13:01:42 +0200

    Rename test1 to ShouldCalculateFactorial

--- ColumbaChallange.MathLib/ColumbaChallange.MathLib.Tests/AbstractMathLibTests.cs
+++ ColumbaChallange.MathLib/ColumbaChallange.MathLib.Tests/AbstractMathLibTests.cs
@@ -16,7 +16,7 @@ namespace ColumbaChallange.MathLib.Tests
         [InlineData(0, 1)]
         //https://www.google.com/search?q=factorial+10&rlz=1C1CHBF_frDE882DE882&oq=factorial+10+&aqs=chrome..69i57.6525j0j7&sourceid=chrome&ie=UTF-8
         [InlineData(10, 3628800)]
-        public void Test1(int n, int expected)
+        public void ShouldCalculateFactorial(int n, int expected)
         {
             var result = MathLib.Factorial(n);
 
