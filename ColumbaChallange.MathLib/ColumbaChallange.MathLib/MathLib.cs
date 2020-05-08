60c4cbf5ba9eac8566ec59e6885d21ff8b661217
yann <yann.hortance@gmail.com>
2020-05-08 13:23:46 +0200

    Refactor Factorial to isolate N striclyPositive check

--- ColumbaChallange.MathLib/ColumbaChallange.MathLib/MathLib.cs
+++ ColumbaChallange.MathLib/ColumbaChallange.MathLib/MathLib.cs
@@ -6,15 +6,22 @@ namespace ColumbaChallange.MathLib
     {
         public long Factorial(int n)
         {
-            if(n < 0)
-            {
-                throw new ArgumentException($"{nameof(n)} cannot be strictly negative.");
-            }
-            if(n == 0)
+            EnsureNStrictlyPositive(n);
+            if (n == 0)
             {
                 return 1;
             }
             return n * Factorial(n - 1);
+        }
+
+
+
+        private void EnsureNStrictlyPositive(int n)
+        {
+            if (n < 0)
+            {
+                throw new ArgumentException($"{nameof(n)} cannot be strictly negative.");
+            }
         }
     }
 }
