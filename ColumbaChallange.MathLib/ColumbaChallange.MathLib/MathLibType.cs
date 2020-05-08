51e898636e04992d1d3a7356d2fad4a651eeba18
yann <yann.hortance@gmail.com>
2020-05-08 13:08:29 +0200

    Create a factory method to choose between Recursive or ListFunction

---
+++ ColumbaChallange.MathLib/ColumbaChallange.MathLib/MathLibType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ColumbaChallange.MathLib
+{
+    public enum MathLibType
+    {
+        Recursive, ListFunction
+    }
+}
