4979c5ad80b09f73848f1b1f48096bf793417fe6
yann <yann.hortance@gmail.com>
2020-05-08 13:29:39 +0200

    Refactor Factorial to use EnsureNStrictlyPositive

--- ColumbaChallange.MathLib/ColumbaChallange.MathLib/MathLibV2.cs
+++ ColumbaChallange.MathLib/ColumbaChallange.MathLib/MathLibV2.cs
@@ -7,11 +7,16 @@ namespace ColumbaChallange.MathLib
     {
         public long Factorial(int n)
         {
+            EnsureNStrictlyPositive(n);
+            return n == 0 ? 1 : Enumerable.Range(1, n).Aggregate((i, j) => i * j);
+        }
+
+        private void EnsureNStrictlyPositive(int n)
+        {
             if (n < 0)
             {
                 throw new ArgumentException($"{nameof(n)} cannot be strictly negative.");
             }
-            return n == 0 ? 1 : Enumerable.Range(1, n).Aggregate((i, j) => i * j);
         }
     }
 }
