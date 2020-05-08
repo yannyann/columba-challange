24552271cd3fde0756e0a183535db0f1bf2f98d8
yann <yann.hortance@gmail.com>
2020-05-08 13:04:55 +0200

    Add one more test Fact(1) = 1

--- ColumbaChallange.MathLib/ColumbaChallange.MathLib.Tests/AbstractMathLibTests.cs
+++ ColumbaChallange.MathLib/ColumbaChallange.MathLib.Tests/AbstractMathLibTests.cs
@@ -14,6 +14,7 @@ namespace ColumbaChallange.MathLib.Tests
 
         [Theory]
         [InlineData(0, 1)]
+        [InlineData(1, 1)]
         //https://www.google.com/search?q=factorial+10&rlz=1C1CHBF_frDE882DE882&oq=factorial+10+&aqs=chrome..69i57.6525j0j7&sourceid=chrome&ie=UTF-8
         [InlineData(10, 3628800)]
         public void ShouldCalculateFactorial(int n, int expected)
